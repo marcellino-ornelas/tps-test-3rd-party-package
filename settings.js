ac58fc939383fe510a00b389d35b9b27763633f0
Marcellino Ornelas <marcellino.ornelas@gmail.com>
2024-03-11 23:52:31 -0700

    prompt

--- settings.js
+++ settings.js
@@ -1,1 +1,11 @@
-module.exports = {};
+module.exports = {
+  prompts: [
+    {
+      name: "prompt1",
+      message: "do you want prompt1",
+      type: "confirm",
+      typeType: "package",
+      default: false,
+    },
+  ],
+};
